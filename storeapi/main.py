030243dc54eb14c7e3f138aa5e9b81d63c87185f
Enzowt05 <enzowt05@gmail.com>
2025-08-06 22:42:20 -0300

    feat: Create a post

--- storeapi/main.py
+++ storeapi/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+post_table = {}
 
 
 @app.get("/")
 async def root():
     return {"message": "test"}
+
+
+@app.post("/", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    return new_post
