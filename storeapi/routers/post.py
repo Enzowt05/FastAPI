01af3220eb54c551149b672cdcab8f96aac41e34
Enzowt05 <enzowt05@gmail.com>
2025-08-09 21:48:41 -0300

    feat(logging): logging to HTTPException handler

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -43,7 +43,6 @@ async def create_post(post: UserPostIn):
 async def create_comment(comment: CommentIn):
     post = await find_post(comment.post_id)
     if not post:
-        logger.error(f"Post with id {comment.post_id} not found")
         raise HTTPException(status_code=404, detail="Post not found.")
 
     data = comment.model_dump()
@@ -65,7 +64,6 @@ async def get_post_with_comments(post_id: int):
     logger.info("Getting post with its comments")
     post = await find_post(post_id)
     if not post:
-        logger.error(f"Post with post id {post_id} not found")
         raise HTTPException(status_code=404, detail="Post not found.")
 
     return {"post": post, "comments": await get_comments_on_posts(post_id)}
