fb63a3072a9da4894c1d070cfa08151cda8d4666
Enzowt05 <enzowt05@gmail.com>
2025-08-16 18:34:35 -0300

    feat(test): test_authenticate_user

--- storeapi/tests/test_security.py
+++ storeapi/tests/test_security.py
@@ -1,16 +1,19 @@
 import pytest
 from jose import jwt
+
 from storeapi import security
+
 
 @pytest.mark.anyio
 async def test_access_token_expire_minutes():
     assert security.access_token_expire_minutes() == 30
 
+
 @pytest.mark.anyio
 async def test_create_access_token():
     token = security.create_access_token("email")
     assert {"sub": "email"}.items() <= jwt.decode(
-        token, key=security.SECRET_KEY, algorithms=[security.ALGORITHM ]
+        token, key=security.SECRET_KEY, algorithms=[security.ALGORITHM]
     ).items()
 
 
@@ -31,3 +34,11 @@ async def test_get_user(registered_user: dict):
 async def test_get_user_not_found():
     user = await security.get_user("test@example.com")
     assert user is None
+
+
+@pytest.mark.anyio
+async def test_authenticate_user(registered_user: dict):
+    user = await security.authenticate_user(
+        registered_user["email"], registered_user["password"]
+    )
+    assert user.email == registered_user["email"]
