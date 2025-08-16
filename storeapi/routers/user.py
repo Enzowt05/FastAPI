42f9d3d26cd7b6364410ecd8475caf8c4bd394a4
Enzowt05 <enzowt05@gmail.com>
2025-08-16 17:33:28 -0300

    fix: register with hash password

--- storeapi/routers/user.py
+++ storeapi/routers/user.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, HTTPException, status
 
 from storeapi.database import database, user_table
 from storeapi.models.user import UserIn
-from storeapi.security import get_user
+from storeapi.security import get_password_hash, get_user
 
 logger = logging.getLogger(__name__)
 
@@ -18,9 +18,10 @@ async def register(user: UserIn):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User with that email already exists",
         )
+    hashed_password = get_password_hash(user.password)
     query = user_table.insert().values(
-        email=user.email, password=user.password
-    )  # While without Hash
+        email=user.email, password=hashed_password
+    )
 
     logger.debug(query)
 
