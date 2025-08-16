564c7fac623ac564e898213ae04025dc9dfe7cc9
Enzowt05 <enzowt05@gmail.com>
2025-08-16 18:54:49 -0300

    feat(test): test_get_current_user_invalid_token

--- storeapi/tests/test_security.py
+++ storeapi/tests/test_security.py
@@ -61,3 +61,9 @@ async def test_get_current_user(registered_user: dict):
     token = security.create_access_token(registered_user["email"])
     user = await security.get_current_user(token)
     assert user.email == registered_user["email"]
+
+
+@pytest.mark.anyio
+async def test_get_current_user_invalid_token():
+    with pytest.raises(security.HTTPException):
+        await security.get_current_user("invalid token")
