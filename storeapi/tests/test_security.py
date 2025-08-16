f3a69c16fb32ca037f113eeb89764fbd685a94b6
Enzowt05 <enzowt05@gmail.com>
2025-08-16 18:32:16 -0300

    feat(test): test_create_access_token

--- storeapi/tests/test_security.py
+++ storeapi/tests/test_security.py
@@ -7,6 +7,14 @@ async def test_access_token_expire_minutes():
     assert security.access_token_expire_minutes() == 30
 
 @pytest.mark.anyio
+async def test_create_access_token():
+    token = security.create_access_token("email")
+    assert {"sub": "email"}.items() <= jwt.decode(
+        token, key=security.SECRET_KEY, algorithms=[security.ALGORITHM ]
+    ).items()
+
+
+@pytest.mark.anyio
 async def test_password_hashes():
     password = "password"
     assert security.verify_password(password, security.get_password_hash(password))
