5203e028ace323b14f465fbc39c518291581ecc1
Enzowt05 <enzowt05@gmail.com>
2025-08-16 16:27:25 -0300

    chore: fastAPI status and code formatting

--- storeapi/routers/user.py
+++ storeapi/routers/user.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 
 from storeapi.models.user import UserIn
 from storeapi.security import get_user
 
 router = APIRouter()
 
+
 @router.post("/register", status_code=201)
 async def register(user: UserIn):
     if await get_user(user.email):
         raise HTTPException(
-            status_code =400,
-            detail="User with that email already exists"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with that email already exists",
         )
--- storeapi/security.py
+++ storeapi/security.py
@@ -4,6 +4,7 @@ from storeapi.database import database, user_table
 
 logger = logging.getLogger(__name__)
 
+
 async def get_user(email: str):
     logging.debug("Fetching user from the database", extra={"email": email})
     query = user_table.select().where(user_table.c.email == email)
