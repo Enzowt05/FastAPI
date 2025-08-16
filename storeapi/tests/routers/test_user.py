99a0b8bfae01de802f6889e6401e8e4753289af7
Enzowt05 <enzowt05@gmail.com>
2025-08-16 17:22:14 -0300

    feat(test): test_register_user

---
+++ storeapi/tests/routers/test_user.py
@@ -0,0 +1,15 @@
+import pytest
+from httpx import AsyncClient
+
+
+async def register_user(async_client: AsyncClient, email: str, password: str):
+    return await async_client.post(
+        "/register", json={"email": email, "password": password}
+    )
+
+
+@pytest.mark.anyio
+async def test_register_user(async_client: AsyncClient):
+    response = await register_user(async_client, "test@example.net", "1234")
+    assert response.status_code == 201
+    assert "User created" in response.json()["detail"]
